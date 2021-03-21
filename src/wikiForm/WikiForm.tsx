f7256d62ce2e926470b9eef5a1db2213f97fd887
camolezi <camolezi@usp.br>
2021-03-21 16:19:53 -0300

    Component only render UI- Created hook for handling fetch logic

---
+++ src/wikiForm/WikiForm.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import useWikiForm from './useWikiForm';
+import { WikiFormActionsTypes } from './WikiFormReducer';
+
+function WikiForm(): JSX.Element {
+  const [text, setText] = useState('');
+  const [, dispatch] = useWikiForm();
+
+  return (
+    <>
+      <label htmlFor="wikiInput">
+        Type something:
+        <input
+          id="wikiInput"
+          type="text"
+          onChange={(value) => setText(value.target.value)}
+        />
+      </label>
+      <br />
+
+      <button
+        type="button"
+        onClick={() =>
+          dispatch({
+            type: WikiFormActionsTypes.CLICKED_SEARCH_BUTTON,
+            payload: text,
+          })
+        }
+      >
+        Search on Wikipedia
+      </button>
+    </>
+  );
+}
+
+export default WikiForm;
---
+++ src/wikiForm/useWikiForm.tsx
@@ -0,0 +1,51 @@
+import React, { useCallback, useReducer } from 'react';
+import WikiFormReducer, {
+  WikiFormActions,
+  WikiFormActionsTypes,
+  WikiFormState,
+} from './WikiFormReducer';
+
+const initialState: WikiFormState = {
+  searchText: '',
+  searchResult: {
+    wordCount: 0,
+    title: '',
+  },
+};
+
+function getUrlWikipedia(keyword: string) {
+  return `https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&format=json&srlimit=1&srsearch=${keyword}`;
+}
+
+async function fetchWikipediaData(keyword: string) {
+  const wikiData = await fetch(getUrlWikipedia(keyword));
+  const data = await wikiData.json();
+  return {
+    wordCount: data.query.search[0].wordcount,
+    title: data.query.search[0].title,
+  };
+}
+
+function useWikiForm(): [WikiFormState, React.Dispatch<WikiFormActions>] {
+  const [wikiForm, dispatch] = useReducer(WikiFormReducer, initialState);
+
+  const dispatchUsingMiddleware = useCallback(
+    async (action: WikiFormActions) => {
+      if (action.type === WikiFormActionsTypes.CLICKED_SEARCH_BUTTON) {
+        fetchWikipediaData(action.payload).then((payload) =>
+          dispatch({
+            type: WikiFormActionsTypes.RECEIVED_WIKIPEDIA_DATA,
+            payload,
+          })
+        );
+      }
+
+      dispatch(action);
+    },
+    [dispatch]
+  );
+
+  return [wikiForm, dispatchUsingMiddleware];
+}
+
+export default useWikiForm;
