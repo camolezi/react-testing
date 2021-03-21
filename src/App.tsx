a1e60f7c43ba943370b174f722a268ff5760183f
camolezi <camolezi@usp.br>
2021-03-21 16:18:22 -0300

    Moved form logic to a isolated component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+import WikiForm from './wikiForm/WikiForm';
 
 function App(): JSX.Element {
-  const [color, setColor] = useState('blue');
-
-  const buttonStyle = {
-    backgroundColor: color,
-    color: 'white',
-    width: '10vw',
-    height: '10vh',
-  };
-
   return (
     <main>
-      <label htmlFor="numberInput">
-        Type a number
-        <input
-          id="numberInput"
-          type="number"
-          onChange={(value) => {
-            if (Number(value.target.value) < 10) setColor('blue');
-            else setColor('red');
-          }}
-        />
-      </label>
-      <br />
-
-      <button type="button" style={buttonStyle}>
-        Color
-      </button>
+      <WikiForm />
     </main>
   );
 }
